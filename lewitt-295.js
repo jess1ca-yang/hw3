d737dc441dc39914af2b66b375184923f5545601
Jessica Yang <jessica.yang@cca.edu>
2018-02-03 12:59:36 -0800

    Create lewitt-295.js

---
+++ lewitt-295.js
@@ -0,0 +1,12 @@
+createCanvas(340, 340);
+
+background("black"); 
+
+noFill();
+stroke("white");
+rect(20, 20, 300, 300);
+ellipse(170, 170, 300, 300);
+triangle(20, 320, 170, 20, 320, 320);
+triangle(20, 320, 85, 24, 85, 320);
+triangle(320, 320, 255, 24, 255, 320);
+line(255, 320, 320, 20);
