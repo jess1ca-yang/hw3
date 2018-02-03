b4bf406f35566fdd9ffdb6abe3fcb1758fdabc1c
Jessica Yang <jessica.yang@cca.edu>
2018-02-03 13:47:09 -0800

    Create drawing-new-colors.js

---
+++ drawing-new-colors.js
@@ -0,0 +1,37 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    stroke(209, 73, 60);
+    fill(249, 100, 77);
+  } else if (key == 'G') {
+    stroke(124, 201, 80);
+    fill(194, 234, 107);
+  } else if (key == 'B') {
+    stroke(59, 135, 206);
+    fill(95, 213, 252); 
+  }
+  else if (key == 'O') {
+    stroke(234, 114, 39);
+    fill(255, 156, 58); 
+  }
+   else if (key == 'P') {
+    stroke(234, 86, 155);
+    fill(255, 117, 183); 
+  }
+  else if (key == 'Y') {
+    stroke(229, 193, 64);
+    fill(255, 247, 104); 
+  }
+}
